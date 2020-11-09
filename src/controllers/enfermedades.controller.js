9eeb999a8c4b74fe97824b47edc6ad9cad74b741
focasr19 <65393583+focasr19@users.noreply.github.com>
2020-11-08 23:24:28 -0500

    x
    x

--- src/controllers/enfermedades.controller.js
+++ src/controllers/enfermedades.controller.js
@@ -1,44 +1,131 @@
-const enfermedadController = {}
-const { request, response } = require('express')
-const db = require('../../db/firebase.conn')
-const brain = require('brain.js')
-const net = new brain.recurrent.LSTM()
+const enfermedadController = {};
+const { request, response } = require("express");
+const db = require("../../db/firebase.conn");
+const brain = require("brain.js");
+const net = new brain.recurrent.LSTM();
+const mimir = require("mimir");
+const { dict } = require("mimir");
 
-enfermedadController.getData = async(req = request, res = response) => {
-    let datos = []
-    const sintomas = await db.collection("EnfermedadesComunes").get()
-    sintomas.forEach((result) => {
-        datos.push(result.data())
-    })
+enfermedadController.getData = async (req = request, res = response) => {
+  let datos = [];
+  //let enfermedad = [];
+  //let datosSintomas = [];
+  //let conjuntoDatos = [];
+  const sintomas = await db.collection("EnfermedadesComunes").get();
+  sintomas.forEach((doc) => {
+    if (!(doc.data().Sintoma in datos)) {
+      datos[doc.data().Sintoma] = true;
+      datos.push({ Sintoma: doc.data().Sintoma });
+      //datosSintomas.push(doc.data().Sintoma);
+    }
+  });
 
-    res.render("index", {
-        data: datos
-    })
-}
+  res.render("index", {
+    data: datos,
+  });
+};
 
-enfermedadController.postData = (req = request, res = response) => {
-    const conjunto = []
-    const data = JSON.parse(JSON.stringify(req.body));
-    conjunto.push(data)
-    conjunto.forEach((sintomas) => {
-        console.log('funciono hpta', sintomas);
-        prediccion(sintomas)
-    })
-    res.redirect("/")
-}
+enfermedadController.postData = async (req = request, res = response) => {
+  let datos = [];
+  let enfermedad = [];
+  let datosSintomas = [];
+  let conjuntoDatos = {};
+  const sintomas = await db.collection("EnfermedadesComunes").get();
+  sintomas.forEach((doc) => {
+    if (!(doc.data().Sintoma in datos)) {
+      datos[doc.data().Sintoma] = true;
+      datos.push({ Sintoma: doc.data().Sintoma });
+      datosSintomas.push(doc.data().Sintoma);
+    }
+    if (!(doc.data().Enfermedad in enfermedad)) {
+      enfermedad[doc.data().Enfermedad] = true;
+      enfermedad.push(doc.data().Enfermedad);
+    }
+    //console.log(data().Sintoma)
+    //datosSintomas.push(doc.data().Sintoma);
+    conjuntoDatos[doc.data().Sintoma] = doc.data().Enfermedad;
+  });
+  //console.log(enfermedad);
+  console.log(datosSintomas);
+  console.log(conjuntoDatos);
 
+  let ANN_Classes = {};
+  for (let index = 0; index < enfermedad.length; index++) {
+    ANN_Classes[enfermedad[index]] = index;
+  }
+  //console.log(ANN_Classes);
+
+  classes_array = Object.keys(ANN_Classes);
+
+  let dict = mimir.dict(datosSintomas);
+  let traindata = [];
+
+  for (keyConjunto in conjuntoDatos) {
+    for (keyANN in ANN_Classes) {
+      if (keyANN == conjuntoDatos[keyConjunto]) {
+          //console.log(keyConjunto+"**-*-*-*-*-*-"+ANN_Classes[keyANN])
+        traindata.push([mimir.bow(keyConjunto, dict), ANN_Classes[keyANN]])
+        break;
+      }
+    }
+  }
+  //console.log(traindata)
+ 
+  //console.log(traindata);
+  const conjunto = [];
+  const data = JSON.parse(JSON.stringify(req.body));
+  conjunto.push(data);
+
+  testEnfermedad = "Presión en la parte inferior del abdomen, Orina con sangre, Dolor al Orinar, Orina turbia, Opresión en los riñones, Fiebre";
+  testEnfermedaddos= "Dolor en los ojos, Sarpullido, Vomito, Dolor en los huesos, Diarrea,Dolor en las articulaciones,Nauseas,Dolor Muscular "
+  testEnfermedadtres = "Tos, Fiebre, Congestión Nasal, Estornudos, Dolor de Garganta"
+  testBowEnfermedad = mimir.bow(testEnfermedad, dict);
+  testBowEnfermedaddos = mimir.bow(testEnfermedaddos, dict)
+  testEnfermedadtresbow = mimir.bow(testEnfermedadtres,dict)
+  
+
+  var net = new brain.NeuralNetwork();
+  ann_train = traindata.map(function (pair) {
+    return {
+      input: pair[0],
+      output: vec_result(pair[1], datosSintomas.length),
+    };
+  });
+  //console.log(ann_train)
+  function vec_result(res, num_classes) {
+    var i = 0,
+      vec = [];
+    for (i; i < num_classes; i += 1) {
+      vec.push(0);
+    }
+    vec[res] = 1;
+    return vec;
+  }
+
+  function maxarg(array) {
+    return array.indexOf(Math.max.apply(Math, array));
+  }
+
+  net.train(ann_train);
+  var predict = net.run(testBowEnfermedad);
+
+  console.log(predict);
+  console.log(classes_array[maxarg(predict)]); // prints HISTORY
+  console.log(classes_array[maxarg(net.run(testBowEnfermedaddos))]);
+  console.log(classes_array[maxarg(net.run(testEnfermedadtresbow))]);
+
+  res.redirect("/");
+};
 
 const prediccion = (sintomas) => {
+  //console.log('desde la funsion predeir', sintomas);
+  // net.train([
+  //     { input: "Gripa", output: 'Resfriado común' }
+  // ])
+  // const output = net.run(`${sintoma}`);
+  // console.log('prediccion', output);
+};
 
-    console.log('desde la funsion predeir', sintomas);
-    // net.train([
-    //     { input: "Gripa", output: 'Resfriado común' }
-    // ])
-    // const output = net.run(`${sintoma}`);
-    // console.log('prediccion', output);
-}
-
-prediccion();
-
+//prediccion();
 
 module.exports = enfermedadController;
